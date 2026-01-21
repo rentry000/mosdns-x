f679f9191b96f9ac34b5900d0a46103b8ea03b9d
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-01-21 10:03:48 +0700

    Update limit_ip.go

--- plugin/executable/limit_ip/limit_ip.go
+++ plugin/executable/limit_ip/limit_ip.go
@@ -2,7 +2,6 @@ package limit_ip
 
 import (
 	"context"
-
 	"github.com/miekg/dns"
 	"github.com/pmkol/mosdns-x/coremain"
 	"github.com/pmkol/mosdns-x/pkg/executable_seq"
@@ -15,8 +14,6 @@ func init() {
 	coremain.RegNewPluginFunc(PluginType, Init, func() interface{} { return new(Args) })
 }
 
-var _ coremain.ExecutablePlugin = (*limitIPPlugin)(nil)
-
 type Args struct {
 	Limit int `yaml:"limit"`
 }
@@ -27,54 +24,37 @@ type limitIPPlugin struct {
 }
 
 func Init(bp *coremain.BP, args interface{}) (coremain.Plugin, error) {
-    return newLimitIPPlugin(bp, args.(*Args)), nil
-}
-
-func newLimitIPPlugin(bp *coremain.BP, args *Args) *limitIPPlugin {
-	limit := args.Limit
+	cfg := args.(*Args)
+	limit := cfg.Limit
 	if limit <= 0 {
 		limit = 3
 	}
-	return &limitIPPlugin{
-		BP:    bp,
-		limit: limit,
-	}
+	return &limitIPPlugin{BP: bp, limit: limit}, nil
 }
 
 func (p *limitIPPlugin) Exec(ctx context.Context, qCtx *query_context.Context, next executable_seq.ExecutableChainNode) error {
-    r := qCtx.R()
-
-    if r == nil || len(r.Answer) <= p.limit {
-        return executable_seq.ExecChainNode(ctx, qCtx, next)
-    }
-
-    w := 0
-    ipCount := 0
-
-    for i, rr := range r.Answer {
-        h := rr.Header().Rrtype
-
-        shouldKeep := true
-
-        if h == dns.TypeA || h == dns.TypeAAAA {
-            if ipCount < p.limit {
-                ipCount++
-            } else {
-                shouldKeep = false
-            }
-        }
-
-        if shouldKeep {
-            if w != i {
-                r.Answer[w] = rr
-            }
-            w++
-        }
-    }
-    
-    if w < len(r.Answer) {
-        r.Answer = r.Answer[:w]
-    }
-
-    return executable_seq.ExecChainNode(ctx, qCtx, next)
+	if err := executable_seq.ExecChainNode(ctx, qCtx, next); err != nil {
+		return err
+	}
+	
+	r := qCtx.R()
+	if r == nil {
+		return nil
+	}
+	
+	filtered := r.Answer[:0]
+	ipCount := 0
+	
+	for _, rr := range r.Answer {
+		if rr.Header().Rrtype == dns.TypeA || rr.Header().Rrtype == dns.TypeAAAA {
+			if ipCount >= p.limit {
+				continue
+			}
+			ipCount++
+		}
+		filtered = append(filtered, rr)
+	}
+	
+	r.Answer = filtered
+	return nil
 }
