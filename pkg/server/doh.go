7bd88b991e1798d17cce0d9207d2c849788cdf06
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-01-25 16:53:34 +0700

    Update doh.go

--- pkg/server/doh.go
+++ pkg/server/doh.go
@@ -27,41 +27,72 @@ import (
 	"time"
 
 	"gitlab.com/go-extension/http"
-
 	H "github.com/pmkol/mosdns-x/pkg/server/http_handler"
+)
+
+const (
+	// Timeout for reading request headers (TLS handshake + HTTP headers)
+	defaultReadHeaderTimeout = 3 * time.Second
+	
+	// Timeout for reading entire request (headers + body + handler processing)
+	// Should be larger than DNS handler timeout (5s) to allow proper handling
+	defaultReadTimeout = 10 * time.Second
+	
+	// Timeout for writing response to client
+	defaultWriteTimeout = 10 * time.Second
+	
+	// Maximum size of request headers (4KB is standard for most web servers)
+	defaultMaxHeaderBytes = 4096
 )
 
 func (s *Server) ServeHTTP(l net.Listener) error {
 	defer l.Close()
-
+	
 	if s.opts.HttpHandler == nil {
 		return errMissingHTTPHandler
 	}
-
+	
+	// IdleTimeout is for keep-alive connections between requests
 	idleTimeout := s.opts.IdleTimeout
 	if idleTimeout == 0 {
 		idleTimeout = defaultTCPIdleTimeout
 	}
-
+	
 	hs := &http.Server{
-		Handler:           &eHandler{s.opts.HttpHandler},
-		ReadHeaderTimeout: time.Millisecond * 500,
-		ReadTimeout:       idleTimeout,
-		IdleTimeout:       idleTimeout,
-		MaxHeaderBytes:    2048,
+		Handler: &eHandler{s.opts.HttpHandler},
+		
+		// ReadHeaderTimeout: Time to read request headers
+		// Must account for TLS handshake (~200-500ms) + header parsing
+		ReadHeaderTimeout: defaultReadHeaderTimeout,
+		
+		// ReadTimeout: Total time to read request and process handler
+		// Must be > DNS handler timeout (5s) to prevent premature timeouts
+		ReadTimeout: defaultReadTimeout,
+		
+		// WriteTimeout: Time to write response to client
+		// Protects against slow clients and ensures resources are freed
+		WriteTimeout: defaultWriteTimeout,
+		
+		// IdleTimeout: Time to keep connection alive between requests (keep-alive)
+		// Can be much longer as connection is idle, not processing
+		IdleTimeout: idleTimeout,
+		
+		// MaxHeaderBytes: Maximum size of request headers
+		// 4KB is sufficient for DNS-over-HTTPS and standard HTTP headers
+		MaxHeaderBytes: defaultMaxHeaderBytes,
 	}
+	
 	if ok := s.trackCloser(hs, true); !ok {
 		return ErrServerClosed
 	}
 	defer s.trackCloser(hs, false)
-
+	
 	err := hs.Serve(l)
-	if err == http.ErrServerClosed { // Replace http.ErrServerClosed with our ErrServerClosed
+	if err == http.ErrServerClosed {
+		// Replace http.ErrServerClosed with our ErrServerClosed
 		return ErrServerClosed
-	} else if err != nil {
-		return err
 	}
-	return nil
+	return err
 }
 
 type eHandler struct {
@@ -83,12 +114,11 @@ func (r *eRequest) URL() *url.URL {
 func (r *eRequest) TLS() *H.TlsInfo {
 	if r.r.TLS == nil {
 		return nil
-	} else {
-		return &H.TlsInfo{
-			Version:            r.r.TLS.Version,
-			ServerName:         r.r.TLS.ServerName,
-			NegotiatedProtocol: r.r.TLS.NegotiatedProtocol,
-		}
+	}
+	return &H.TlsInfo{
+		Version:            r.r.TLS.Version,
+		ServerName:         r.r.TLS.ServerName,
+		NegotiatedProtocol: r.r.TLS.NegotiatedProtocol,
 	}
 }
 
