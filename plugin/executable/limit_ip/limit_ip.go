dee218f3b54cba8118dcb9517e1bff3adb011406
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-01-26 20:59:20 +0700

    apply Fisher-Yates shuffle algorithm to randomize IP order

--- plugin/executable/limit_ip/limit_ip.go
+++ plugin/executable/limit_ip/limit_ip.go
@@ -2,7 +2,11 @@ package limit_ip
 
 import (
 	"context"
+	"math/rand"
+	"sync"
+
 	"github.com/miekg/dns"
+
 	"github.com/pmkol/mosdns-x/coremain"
 	"github.com/pmkol/mosdns-x/pkg/executable_seq"
 	"github.com/pmkol/mosdns-x/pkg/query_context"
@@ -11,7 +15,9 @@ import (
 const PluginType = "limit_ip"
 
 func init() {
-	coremain.RegNewPluginFunc(PluginType, Init, func() interface{} { return new(Args) })
+	coremain.RegNewPluginFunc(PluginType, Init, func() interface{} {
+		return new(Args)
+	})
 }
 
 type Args struct {
@@ -21,6 +27,8 @@ type Args struct {
 type limitIPPlugin struct {
 	*coremain.BP
 	limit int
+
+	shuffleMutex sync.Mutex
 }
 
 func Init(bp *coremain.BP, args interface{}) (coremain.Plugin, error) {
@@ -29,32 +37,55 @@ func Init(bp *coremain.BP, args interface{}) (coremain.Plugin, error) {
 	if limit <= 0 {
 		limit = 3
 	}
-	return &limitIPPlugin{BP: bp, limit: limit}, nil
+	return &limitIPPlugin{
+		BP:    bp,
+		limit: limit,
+	}, nil
 }
 
-func (p *limitIPPlugin) Exec(ctx context.Context, qCtx *query_context.Context, next executable_seq.ExecutableChainNode) error {
+func (p *limitIPPlugin) shuffle(rrs []dns.RR) {
+	p.shuffleMutex.Lock()
+	defer p.shuffleMutex.Unlock()
+	rand.Shuffle(len(rrs), func(i, j int) {
+		rrs[i], rrs[j] = rrs[j], rrs[i]
+	})
+}
+
+func (p *limitIPPlugin) Exec(
+	ctx context.Context,
+	qCtx *query_context.Context,
+	next executable_seq.ExecutableChainNode,
+) error {
+
 	if err := executable_seq.ExecChainNode(ctx, qCtx, next); err != nil {
 		return err
 	}
-	
+
 	r := qCtx.R()
 	if r == nil {
 		return nil
 	}
-	
-	filtered := r.Answer[:0]
-	ipCount := 0
-	
+
+	var ipRRs []dns.RR
+	var otherRRs []dns.RR
+
 	for _, rr := range r.Answer {
-		if rr.Header().Rrtype == dns.TypeA || rr.Header().Rrtype == dns.TypeAAAA {
-			if ipCount >= p.limit {
-				continue
-			}
-			ipCount++
+		switch rr.Header().Rrtype {
+		case dns.TypeA, dns.TypeAAAA:
+			ipRRs = append(ipRRs, rr)
+		default:
+			otherRRs = append(otherRRs, rr)
 		}
-		filtered = append(filtered, rr)
 	}
-	
-	r.Answer = filtered
+
+	if len(ipRRs) > 1 {
+		p.shuffle(ipRRs)
+	}
+
+	if len(ipRRs) > p.limit {
+		ipRRs = ipRRs[:p.limit]
+	}
+
+	r.Answer = append(otherRRs, ipRRs...)
 	return nil
 }
