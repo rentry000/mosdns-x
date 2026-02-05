29d5867741e384cd2a19e4647a8fdda988f90dec
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-02-06 04:11:15 +0700

    Refactor IntMatcher to use matcher interface

--- pkg/matcher/elem/rr_type.go
+++ pkg/matcher/elem/rr_type.go
@@ -19,21 +19,110 @@
 
 package elem
 
-type IntMatcher struct {
-	m map[int]struct{}
+type matcher interface {
+	Match(int) bool
 }
 
-// NewIntMatcher inits a new IntMatcher.
-func NewIntMatcher(elem []int) *IntMatcher {
-	matcher := &IntMatcher{m: make(map[int]struct{})}
+type bitmask64Matcher uint64
 
-	for _, v := range elem {
-		matcher.m[v] = struct{}{}
+func (b bitmask64Matcher) Match(v int) bool {
+	if uint(v) > 63 {
+		return false
 	}
-	return matcher
+	return (uint64(b) & (1 << v)) != 0
 }
 
-func (m *IntMatcher) Match(v int) bool {
-	_, ok := m.m[v]
+type bitmask128Matcher [2]uint64
+
+func (b *bitmask128Matcher) Match(v int) bool {
+	if uint(v) > 127 {
+		return false
+	}
+
+	idx := v >> 6
+	bit := v & 63
+
+	return (b[idx] & (1 << bit)) != 0
+}
+
+type sliceMatcher []int
+
+func (s sliceMatcher) Match(v int) bool {
+	for _, item := range s {
+		if item == v {
+			return true
+		}
+	}
+	return false
+}
+
+type mapMatcher map[int]struct{}
+
+func (m mapMatcher) Match(v int) bool {
+	_, ok := m[v]
 	return ok
+}
+
+type IntMatcher struct {
+	impl matcher
+}
+
+func (im *IntMatcher) Match(v int) bool {
+	return im.impl.Match(v)
+}
+
+func NewIntMatcher(elem []int) *IntMatcher {
+	return &IntMatcher{
+		impl: newMatcherImpl(elem),
+	}
+}
+
+func newMatcherImpl(elem []int) matcher {
+	if len(elem) == 0 {
+		return sliceMatcher{}
+	}
+
+	var maxVal int
+	var minVal int
+
+	for i, v := range elem {
+		if i == 0 {
+			minVal, maxVal = v, v
+		} else {
+			if v > maxVal {
+				maxVal = v
+			}
+			if v < minVal {
+				minVal = v
+			}
+		}
+	}
+
+	if minVal >= 0 && maxVal <= 63 {
+		var mask uint64
+		for _, v := range elem {
+			mask |= 1 << v
+		}
+		return bitmask64Matcher(mask)
+	}
+
+	if minVal >= 0 && maxVal <= 127 {
+		m := new(bitmask128Matcher)
+		for _, v := range elem {
+			m[v>>6] |= 1 << (v & 63)
+		}
+		return m
+	}
+
+	if len(elem) <= 16 {
+		s := make(sliceMatcher, len(elem))
+		copy(s, elem)
+		return s
+	}
+
+	m := make(mapMatcher, len(elem))
+	for _, v := range elem {
+		m[v] = struct{}{}
+	}
+	return m
 }
